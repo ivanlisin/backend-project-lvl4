49950408613082726839cd20e87c6ea635f5e5d9
Ivan Lisin <77109733+ivanlisin@users.noreply.github.com>
2021-08-15 18:50:35 +0700

    update webpack config

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -11,6 +11,7 @@ module.exports = {
   },
   devServer: {
     host: 'localhost',
+    contentBase: path.join(__dirname, 'dist', 'public'),
     publicPath: '/assets/',
     port: 8080,
     compress: true,
