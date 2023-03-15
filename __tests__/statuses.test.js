c78bca83c1036c264e66e09cbe06978bb58f11c1
Ivan Lisin <ivanlisin.tech@gmail.com>
2023-03-15 16:49:58 +0700

    add test for statuses

---
+++ __tests__/statuses.test.js
@@ -0,0 +1,100 @@
+// @ts-check
+
+import fastify from 'fastify';
+
+import init from '../server/plugin.js';
+import { getTestData, prepareData } from './helpers/index.js';
+
+describe('test statuses CRUD', () => {
+  let app;
+  let knex;
+  let models;
+  let responseSignIn;
+  let cookies;
+  const testData = getTestData();
+
+  beforeAll(async () => {
+    app = fastify({
+      exposeHeadRoutes: false,
+      logger: { target: 'pino-pretty' },
+    });
+    await init(app);
+    knex = app.objection.knex;
+    models = app.objection.models;
+
+    // TODO: пока один раз перед тестами
+    // тесты не должны зависеть друг от друга
+    // перед каждым тестом выполняем миграции
+    // и заполняем БД тестовыми данными
+    await knex.migrate.latest();
+    await prepareData(app);
+
+    responseSignIn = await app.inject({
+      method: 'POST',
+      url: app.reverse('session'),
+      payload: {
+        data: testData.users.existing,
+      },
+    });
+
+    const [sessionCookie] = responseSignIn.cookies;
+    const { name, value } = sessionCookie;
+    cookies = { [name]: value };
+  });
+
+  beforeEach(async () => {
+  });
+
+  it('index', async () => {
+    const response = await app.inject({
+      method: 'GET',
+      url: app.reverse('statuses'),
+      payload: {
+        data: testData.users.existing,
+      },
+      cookies,
+    });
+
+    expect(response.statusCode).toBe(200);
+  });
+
+  it('new', async () => {
+    const response = await app.inject({
+      method: 'GET',
+      url: app.reverse('newStatus'),
+      cookies,
+    });
+
+    expect(response.statusCode).toBe(200);
+  });
+
+  it('create', async () => {
+    const params = testData.statuses.new;
+    const response = await app.inject({
+      method: 'POST',
+      url: app.reverse('statuses'),
+      payload: {
+        data: params,
+      },
+      cookies,
+    });
+
+    expect(response.statusCode).toBe(302);
+    console.log(params);
+    const expected = {
+      ...params,
+    };
+    const status = await models.status.query().findOne({ name: params.name });
+    expect(status).toMatchObject(expected);
+  });
+
+  afterEach(async () => {
+    // Пока Segmentation fault: 11
+    // после каждого теста откатываем миграции
+    // await knex.migrate.rollback();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+});
