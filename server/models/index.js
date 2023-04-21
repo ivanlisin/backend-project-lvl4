b3d1451906ba410ade5011c8efe6c8e4ee88e840
Ivan Lisin <ivanlisin.tech@gmail.com>
2023-04-21 16:04:42 +0700

    add label model

--- server/models/index.js
+++ server/models/index.js
@@ -2,10 +2,12 @@
 
 import User from './User.cjs';
 import Status from './Status.cjs';
+import Label from './Label.cjs';
 import Task from './Task.cjs';
 
 export default [
   User,
   Status,
+  Label,
   Task,
 ];
